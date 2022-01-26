5c8ba5d10705f2d0c3bdd31354b088999a64196e
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-01-26 17:08:56 +0600

    customSearchResult object replace by Array to store 10 response.

--- CustomSearch/Services/CustomSearchService.swift
+++ CustomSearch/Services/CustomSearchService.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CustomSearchService {
     static let shared = CustomSearchService()
-    var customSearch = CustomSearch()
+    var customSearchResults = [CustomSearch?](repeating: nil, count: 10)
     
     private init(){
         
@@ -18,39 +18,40 @@ class CustomSearchService {
     
     func callCustomSearchAPI(q: String, si: Int, num: Int, completionHandler: @escaping (Bool) -> Void ) {
         print("callCustomSearchAPI() called with start=\(si), num=\(num)\n")
-        let urlString = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBhDq-e6q0W3aImfJoaABG37vN-LVlx4J8&num=\(num)&cx=6e6d97019e0665110&start=\(si)&q=\(q)"
+        // two keys [AIzaSyBnxPnj_0vwyaDW4Ka_pDRIyEjAy8-4YyA, AIzaSyBhDq-e6q0W3aImfJoaABG37vN-LVlx4J8]
+        let urlString = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBnxPnj_0vwyaDW4Ka_pDRIyEjAy8-4YyA&num=\(num)&cx=6e6d97019e0665110&start=\(si)&q=\(q)"
         
         if let escapedURLString = urlString.addingPercentEncoding(withAllowedCharacters: Foundation.CharacterSet.urlQueryAllowed), let url = URL(string: escapedURLString) {
             
-//            DispatchQueue.global(qos: .userInitiated).async {
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             
             let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-//                print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
+                print("\(si)th response = ", terminator: "")
+                print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
                 if error == nil && data != nil {
                     // Parse JSON
                     let decoder = JSONDecoder()
                     do {
                         let searchResult = try decoder.decode(CustomSearch.self, from: data!)
 //                        print(searchResult)
-                        self.customSearch = searchResult
+                        self.customSearchResults[si/10] = searchResult
                         completionHandler(true)
                     }
                     catch {
-                        debugPrint(error)
                         print("Error in JSON parsing inside callCustomSearchAPI\n")
+                        completionHandler(false)
                     }
                 }
-                else{
+                else {
+                    print("Found error response from API\n")
                     completionHandler(false)
                 }
             })
             
             task.resume()
-//            }
         }
         else {
             print("Error in api url\n")
